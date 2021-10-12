ccc6f7213617f9017353a5e9cb3aff2d6b7ea45b
Laurentiu <lcostin40@gmail.com>
2021-10-12 17:06:12 +0200

    Aggiunta la funzione per chiudere il menu. (Terminato l'esercizio)

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
+//Selezioniamo la classe da dovre cliccare
+var hamburger = document.querySelector("div > a");
 
-const hamburger = document.querySelector("div > a");
+//Selezioniamo la classe a cui cambiarà la proprietà display
+var apriChiudi = document.querySelector(".hamburger-menu");
 
-//Nel momento in cui si clicca sull'hamburger questo si aprirà
+//Nel momento in cui si clicca sull'hamburger il menu si aprirà
 hamburger.addEventListener(`click`, function () {
-    document.getElementsByClassName("hamburger-menu")[0].style.display = "block";
+    apriChiudi.style.display = "block";
 })
 
 
+//Selezioniamo la classe da dover cliccare
+var chiudi = document.querySelector(".close")
+
+//Nel momento in cui si clicca sulla x il menu verrà chiuso
+chiudi.addEventListener(`click`, function () {
+    apriChiudi.style.display = "none";
+})
 
 
